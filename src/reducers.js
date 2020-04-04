4b46f73e2417572be85c6c4bc3fdc1422af56366
Mario David <mariodaviduzcategui@gmail.com>
2020-04-04 11:37:54 -0400

    more new changes

--- src/reducers.js
+++ src/reducers.js
@@ -1,30 +1,30 @@
 //import initialState from './inicialState';
 const initialState = {
-    like: [10] 
-        
-  }
- 
- 
+    like: [10]
+
+}
+
+
 export default function modificarSaves(state = initialState, action) {
-    switch(action.type) {
-         
+    switch (action.type) {
+
         /* Add contacts to the state array */
-      
+
         case "MODIFICAR_SAVES": {
-            
+
             return Object.assign({}, state, {
                 like: state.like.concat(action.payload)
-              });
-                
-                
-            
+            });
+
+
+
         }
-         
-        
-        
- 
-        
- 
+
+
+
+
+
+
         default: return state;
     }
 }
