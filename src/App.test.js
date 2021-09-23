85c7c8dfa9a3d3d55828e9e976e7e94550d9e12d
mariodavid <mariodaviduzcategui@gmail.com>
2021-09-23 05:51:42 -0500

    prueba-workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Prueba from './Componentes/Prueba'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe("Prueba general", () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Prueba />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 })
+
